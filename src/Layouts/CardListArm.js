fe355f430f2fd00c7073a4eedc231c7aa4a8e0ce
Ana Sofia Ricardo Aguilar <anasofiaricardoaguilar@gmail.com>
2023-05-24 16:41:51 -0500

    24/05/2023  4:43

---
+++ src/Layouts/CardListArm.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Cards from "../Ui/Cards";
+
+const CardListArm = () => {
+    const armData = [
+        {
+            image: 'https://res.cloudinary.com/duxhiuugx/image/upload/v1684872003/gymA_txhmrr.jpg',            
+            title: 'Rutina de Brazos (Biceps y Triceps)',
+            description: 'GymTopz'
+        },
+        {
+            image: 'https://res.cloudinary.com/duxhiuugx/image/upload/v1684872003/gymA_txhmrr.jpg',            
+            title: 'Rutina de Brazos Explosivos',
+            description: 'GymTopz'
+        },
+        {
+            image: 'https://res.cloudinary.com/duxhiuugx/image/upload/v1684872003/gymA_txhmrr.jpg',            
+            title: 'Rutina de Brazos',
+            description: 'GymTopz'
+        },
+        {
+            image: 'https://res.cloudinary.com/duxhiuugx/image/upload/v1684872003/gymA_txhmrr.jpg',            
+            title: 'Rutina de Gluteos',
+            description: 'gymvirtual'
+        }
+    ]
+}
